a67ecb5d2fba1fa9897c8384cb56803e8183eb04
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-26 19:51:22 +0400

    Recover capsule killer event

--- src/board/capmove.c
+++ src/board/capmove.c
@@ -1,3 +1,34 @@
+#include "game/gamework.h"
+#include "game/object.h"
+#include "game/process.h"
+
+typedef struct {
+    int playerNo;
+    int masuId;
+    int capsuleNo;
+} CAPWORK;
+
+void mbev_CapWait(CAPWORK *work);
+BOOL mbCapEffUseCreate(int playerNo, int capsuleNo);
+void mbev_CapRandomBonusCoin(int playerNo, int capsuleNo, BOOL waitF);
+int mbCapEffUseModeGet(int playerNo);
+
+void mbev_CapKiller(void)
+{
+    CAPWORK *work = HuPrcCurrentGet()->property;
+
+    mbev_CapWait(work);
+    mbCapEffUseCreate(work->playerNo, work->capsuleNo);
+    omVibrate(work->playerNo, 20, 4, 4);
+    HuPrcSleep(20);
+    mbev_CapRandomBonusCoin(work->playerNo, work->capsuleNo, TRUE);
+    while (mbCapEffUseModeGet(work->playerNo) >= 0) {
+        HuPrcVSleep();
+    }
+    GwPlayer[work->playerNo].diceMode = 5;
+    HuPrcEnd();
+}
+
 void mbev_CapKinokoKill(void)
 {
 }
