f761a712403fdbf965f2d3141e3a21a0a3d8a474
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-26 19:55:45 +0400

    Recover SNPC chance table helpers

--- src/board/snpc.c
+++ src/board/snpc.c
@@ -1,4 +1,5 @@
 #include "game/board/main.h"
+#include "game/board/masu.h"
 
 #define SNPC_MAGIC 0x534E5043
 
@@ -33,4 +34,67 @@ int mbSNpcMasuGet(void)
 
 static void SNpcStarFunc(void)
 {
+}
+
+void mbMasuChanceKill(void *work)
+{
+    HuMemDirectFree(work);
+}
+
+void mbMasuChanceTypeSet(u8 *chanceTbl, u8 value, int *typeTbl, BOOL inverseF)
+{
+    int masuNum;
+    int masuType;
+    BOOL inverseWork;
+    int i;
+    u8 *chanceTblP;
+    int typeNo;
+
+    masuNum = mbMasuNumGet();
+    inverseWork = inverseF ? TRUE : FALSE;
+    chanceTblP = chanceTbl + 1;
+    for (i = 1; i < masuNum; i++, chanceTblP++) {
+        if (*chanceTblP == 0) {
+            masuType = mbMasuGet(i)->type;
+            for (typeNo = 0; typeTbl[typeNo] >= 0; typeNo++) {
+                if (masuType == typeTbl[typeNo]) {
+                    break;
+                }
+            }
+            if (inverseWork == (typeTbl[typeNo] < 0)) {
+                *chanceTblP = (u8)value;
+            }
+        }
+    }
+}
+
+void mbMasuChanceFlagSet(u8 *chanceTbl, u8 value, u32 flag, u32 mAttr,
+    BOOL inverseF)
+{
+    u8 *chanceTblP;
+    int masuNum;
+    BOOL inverseWork;
+    int i;
+    MASU *masu;
+
+    masuNum = mbMasuNumGet();
+    inverseWork = inverseF ? TRUE : FALSE;
+    chanceTblP = chanceTbl + 1;
+    for (i = 1; i < masuNum; i++, chanceTblP++) {
+        if (*chanceTblP == 0) {
+            masu = mbMasuGet(i);
+            if (inverseWork == (((masu->flag & flag) | (masu->mAttr & mAttr)) == 0)) {
+                *chanceTblP = (u8)value;
+            }
+        }
+    }
+}
+
+void mbMasuChancePlayerSet(u8 *chanceTbl, int value)
+{
+    int i;
+
+    for (i = 0; i < GW_PLAYER_MAX; i++) {
+        chanceTbl[GwPlayer[i].masuId] = value;
+    }
 }
