00d89143ccf1b7ac105e1b0134b94b0f84f8e17c
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-28 21:38:09 +0400

    Narrow mdbank tail type closure

--- src/REL/mdbankdll/mdbank_tail.c
+++ src/REL/mdbankdll/mdbank_tail.c
@@ -1,13 +1,11 @@
-#include "dolphin.h"
-#include "game/hu3d.h"
-#include "game/sprite.h"
+#include <dolphin/mtx/GeoTypes.h>
 
-extern const HuVecF lbl_1_rodata_148;
-extern HUSPR_GROUPID lbl_1_bss_194E[5];
+extern const Vec lbl_1_rodata_148;
+extern s16 lbl_1_bss_194E[5];
 
-void fn_1_2CD8(s16 index, HuVecF *worldPos, float offsetX, float offsetY)
+void fn_1_2CD8(s16 index, Vec *worldPos, float offsetX, float offsetY)
 {
-    HuVecF screenPos = lbl_1_rodata_148;
+    Vec screenPos = lbl_1_rodata_148;
 
     if (worldPos) {
         Hu3D3Dto2D(worldPos, 1, &screenPos);
