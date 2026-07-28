b9a3ab76880054e64cf087d86c7335074828da1f
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-28 15:59:22 +0400

    Recover mdsing transition helpers

--- src/REL/mdsingdll/mdsing.c
+++ src/REL/mdsingdll/mdsing.c
@@ -1,7 +1,103 @@
 #include "dolphin/mtx.h"
+#include "dolphin/os.h"
+#include "game/armem.h"
+#include "game/charman.h"
+#include "game/object.h"
+#include "game/process.h"
 
 extern const float lbl_1_rodata_5C;
 extern const float lbl_1_rodata_60;
+extern s16 lbl_1_bss_34;
+extern s16 lbl_1_bss_1308[];
+extern s16 lbl_1_bss_1340[];
+extern char lbl_1_data_95A[];
+extern char lbl_1_data_96B[];
+extern char lbl_1_data_97D[];
+extern char lbl_1_data_98F[];
+extern char lbl_1_data_99D[];
+extern char lbl_1_data_9A3[];
+extern char lbl_1_data_9D3[];
+extern char lbl_1_data_A02[];
+
+void fn_1_EA0(void)
+{
+    OMOVLHIS *history;
+
+    do {
+        HuPrcVSleep();
+    } while (lbl_1_bss_34 == 0);
+
+    history = omOvlHisGet(0);
+    omOvlHisChg(0, history->ovl, 1, lbl_1_bss_1308[11]);
+    OSReport(lbl_1_data_9A3);
+    OSReport(lbl_1_data_95A, 0x21);
+    OSReport(lbl_1_data_96B, 0x24);
+    OSReport(lbl_1_data_97D, 0x9B);
+    OSReport(lbl_1_data_98F, 0xF2);
+    HuAMemDump();
+    OSReport(lbl_1_data_99D);
+    switch (lbl_1_bss_1340[1]) {
+        case 0:
+            omOvlCallEx(0x72, 1, 0, 0);
+            break;
+        case 1:
+            omOvlCallEx(0x73, 1, 0, 0);
+            break;
+        case 2:
+            omOvlCallEx(0x74, 1, 0, 0);
+            break;
+    }
+}
+
+void fn_1_FEC(void)
+{
+    OMOVLHIS *history;
+
+    do {
+        HuPrcVSleep();
+    } while (lbl_1_bss_34 == 0);
+
+    history = omOvlHisGet(0);
+    omOvlHisChg(0, history->ovl, 2, 0);
+    OSReport(lbl_1_data_9A3);
+    OSReport(lbl_1_data_95A, 0x21);
+    OSReport(lbl_1_data_96B, 0x24);
+    OSReport(lbl_1_data_97D, 0x9B);
+    OSReport(lbl_1_data_98F, 0xF2);
+    HuAMemDump();
+    OSReport(lbl_1_data_99D);
+    omOvlCallEx(0x82, 1, 0, 0);
+}
+
+void fn_1_10D0(void)
+{
+    CharDataClose(-1);
+    HuARDirFree(0x50000);
+    HuARDirFree(0x60000);
+    HuARDirFree(0xC0000);
+    OSReport(lbl_1_data_9D3);
+    OSReport(lbl_1_data_95A, 0x21);
+    OSReport(lbl_1_data_96B, 0x24);
+    OSReport(lbl_1_data_97D, 0x9B);
+    OSReport(lbl_1_data_98F, 0xF2);
+    HuAMemDump();
+    OSReport(lbl_1_data_99D);
+}
+
+void fn_1_1180(void)
+{
+    CharDataClose(-1);
+    HuARDirFree(0x50000);
+    HuARDirFree(0x60000);
+    HuARDirFree(0xC0000);
+    OSReport(lbl_1_data_A02);
+    OSReport(lbl_1_data_95A, 0x21);
+    OSReport(lbl_1_data_96B, 0x24);
+    OSReport(lbl_1_data_97D, 0x9B);
+    OSReport(lbl_1_data_98F, 0xF2);
+    HuAMemDump();
+    OSReport(lbl_1_data_99D);
+}
 
 float fn_1_1230(float start, float control, float end, float weight)
 {
