f08ee88a9b9bc7e1512eb3726df33148bacc0f3b
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-28 08:49:21 +0400

    Recover treetop capsule space objects

--- src/board/capselect.c
+++ src/board/capselect.c
@@ -1,6 +1,15 @@
 #include "game/board/main.h"
 
 #include "game/board/masu.h"
+#include "game/board/object.h"
+
+#include "game/frand.h"
+#include "game/memory.h"
+
+#include "math.h"
+#include "string.h"
+
+#define M_PI 3.141592653589793
 
 extern s8 mbPlayerCapsuleGet(int playerNo, int capsuleNo);
 extern int mbPlayerCapsuleNumGet(int playerNo);
@@ -17,8 +26,21 @@ static BOOL ev_CapMasuDispF;
 static int ev_CapSelectMdlId;
 static int ev_CapSelectValue;
 
+typedef struct CapMasuWork_s {
+    int objNo;
+    int masuId;
+    int modelId;
+    int angle;
+    int unk10;
+    BOOL unk14;
+    float scale;
+    HuVecF pos;
+} CAPMASUWORK;
+
 void mbCapMasuObjCreateAll(void);
 void mbCapMasuObjCreate(int masuId);
+
+static void CapMasuOMExec(OMOBJ *obj);
 
 void mbCapSelectResultSet(int playerNo, int objId, int result)
 {
@@ -159,7 +181,102 @@ void mbCapMasuObjCreateAll(void)
     }
 }
 
+void mbCapMasuObjCreate(int masuId)
+{
+    OMOBJ *obj;
+    CAPMASUWORK *work;
+    int objNo;
+    int i;
+    MBMODELID modelId;
+
+    for (objNo = 0; objNo < 16; objNo++) {
+        if (ev_CapMasuOMObj[objNo] == NULL) {
+            break;
+        }
+    }
+    obj = ev_CapMasuOMObj[objNo] = omAddObjEx(mbObjMan, 0x104, 0, 0, -1,
+        CapMasuOMExec);
+    work = obj->data = HuMemDirectMallocNum(HEAP_HEAP, sizeof(CAPMASUWORK),
+        HU_MEMNUM_OVL);
+    memset(work, 0, sizeof(CAPMASUWORK));
+    work->objNo = objNo;
+    work->masuId = masuId;
+    work->modelId = mbObjCreate(DATANUM(DATA_capsule, 0x21), NULL, TRUE);
+    mbObjLayerSet(work->modelId, 3);
+    modelId = work->modelId;
+    mbObjAttrSet(modelId, HU3D_MOTATTR_LOOP);
+    work->angle = frand() & 0x7FFF;
+    work->unk10 = -1;
+    work->unk14 = FALSE;
+    work->scale = 1.0f;
+    mbMasuPosGet(work->masuId, &work->pos);
+    mbObjPosSetV(work->modelId, &work->pos);
+    for (i = 0; i < GW_PLAYER_MAX; i++) {
+        if (work->masuId == GwPlayer[i].masuId) {
+            break;
+        }
+    }
+    if (i < GW_PLAYER_MAX || GwSystem.turnNo >= GwSystem.turnMax) {
+        work->unk14 = TRUE;
+        work->scale = 0.0f;
+        mbObjDispSet(work->modelId, FALSE);
+    }
+}
+
 void mbCapMasuDispSet(BOOL dispF)
 {
     ev_CapMasuDispF = dispF;
+}
+
+static void CapMasuOMExec(OMOBJ *obj)
+{
+    CAPMASUWORK *work = omObjGetDataAs(obj, CAPMASUWORK);
+    int i;
+
+    if (mbExitCheck() || ev_CapMasuOMObj[work->objNo] == NULL) {
+        omDelObjEx(mbObjMan, obj);
+        return;
+    }
+    if (work->unk14) {
+        for (i = 0; i < GW_PLAYER_MAX; i++) {
+            if (work->masuId == GwPlayer[i].masuId) {
+                break;
+            }
+        }
+        if (i >= GW_PLAYER_MAX && GwSystem.turnNo < GwSystem.turnMax
+            && ev_CapMasuDispF) {
+            work->scale += 0.033333335f;
+            mbObjDispSet(work->modelId, TRUE);
+            if (work->scale >= 1.0f) {
+                work->unk14 = FALSE;
+                work->scale = 1.0f;
+            }
+        }
+    }
+    if (GwSystem.turnNo >= GwSystem.turnMax) {
+        ev_CapMasuDispF = FALSE;
+    }
+    for (i = 0; i < GW_PLAYER_MAX; i++) {
+        if (work->masuId == GwPlayer[i].masuIdNext
+            && GwSystem.turnPlayerNo != i) {
+            break;
+        }
+    }
+    if (i < GW_PLAYER_MAX || !ev_CapMasuDispF) {
+        work->scale -= 0.06666667f;
+        mbObjDispSet(work->modelId, TRUE);
+        if (work->scale <= 0.0f) {
+            mbObjDispSet(work->modelId, FALSE);
+            work->unk14 = TRUE;
+            work->scale = 0.0f;
+        }
+    }
+    mbMasuPosGet(work->masuId, &work->pos);
+    work->pos.y += (100.0f * work->scale)
+        + (work->scale * (10.0 * sin((M_PI * work->angle) / 180.0)));
+    mbObjPosSetV(work->modelId, &work->pos);
+    mbObjScaleSet(work->modelId, work->scale, work->scale, work->scale);
+    if ((work->angle += 2) > 360) {
+        work->angle -= 360;
+    }
 }
