02ff9988cad2d9922b92f86c5ecaf5ab1b4882a1
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-28 23:17:02 +0400

    Register mdsing window and mdbank sprite helpers

--- configure.py
+++ configure.py
@@ -1220,6 +1220,7 @@ config.libs = [
             Object(Matching, "REL/mdsingdll/mdsing_tail15.c"),
             Object(Matching, "REL/mdsingdll/mdsing_tail16.c"),
             Object(Matching, "REL/mdsingdll/mdsing_tail17.c"),
+            Object(Matching, "REL/mdsingdll/mdsing_tail18.c"),
             Object(
                 Matching,
                 "REL/mdsingdll/runtime.c",
@@ -1311,6 +1312,7 @@ config.libs = [
             Object(Matching, "REL/mdbankdll/mdbank_tail.c"),
             Object(Matching, "REL/mdbankdll/mdbank_tail2.c"),
             Object(Matching, "REL/mdbankdll/mdbank_tail3.c"),
+            Object(Matching, "REL/mdbankdll/mdbank_tail4.c"),
             Object(
                 Matching,
                 "REL/mdbankdll/runtime.c",
