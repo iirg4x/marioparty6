af0c756eaeef66ccb8eaf1153397bbbf0310be96
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-29 16:43:45 +0400

    Recover final authenticated Staff helpers

---
+++ src/REL/staffdll/application_pass5_0610.c
@@ -0,0 +1,10 @@
+#include <dolphin/mtx/GeoTypes.h>
+
+typedef Vec HuVecF;
+
+void fn_1_610(HuVecF *dst, float x, float y, float z)
+{
+    dst->x = x;
+    dst->y = y;
+    dst->z = z;
+}
---
+++ src/REL/staffdll/application_pass5_0f44.c
@@ -0,0 +1,8 @@
+#include <dolphin/types.h>
+
+void Hu3DCameraKill(u32 cameraBit);
+
+void fn_1_F44(void)
+{
+    Hu3DCameraKill(1);
+}
